17d32f4acdc64a22b77d01aa08dac9e5eede2b65
Nicolas Judd <nbj2008@nyu.edu>
2024-03-24 20:56:58 -0400

    Added Parrot Path

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
-use std::net::SocketAddr;
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, StatusCode};
+use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 /// This is our service handler. It receives a Request, routes on its
@@ -21,6 +21,12 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
 
             let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
             Ok(Response::new(Body::from(reversed_body)))
+        }
+        (&Method::POST, "/parrot") => {
+            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
+            let parrot_body = ["You said:".as_bytes(), &whole_body].concat();
+
+            Ok(Response::new(Body::from(parrot_body)))
         }
 
         // Return the 404 Not Found for other routes.
@@ -42,7 +48,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let (stream, _) = listener.accept().await?;
 
         tokio::task::spawn(async move {
-            if let Err(err) = Http::new().serve_connection(stream, service_fn(handle_request)).await {
+            if let Err(err) = Http::new()
+                .serve_connection(stream, service_fn(handle_request))
+                .await
+            {
                 println!("Error serving connection: {:?}", err);
             }
         });
